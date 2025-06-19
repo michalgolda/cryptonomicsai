f03f7513d1a5c232a87dc2c3cea3b9e95d2a3858
goldamichal <michal.golda@ziomax.pl>
2025-06-19 22:45:03 +0200

    feat: generate summary using llm

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import json
+import openai
 from adapters.market_data import CoinLoreMarketDataAdapter
 from indicators.dilution_risk import dilution_risk_indicator
 from indicators.market_cap import market_cap_indicator
@@ -5,21 +8,43 @@ from indicators.unit_price import unit_price_indicator
 from constants.asset import Asset
 
 
+openai_client = openai.OpenAI()
+
+
+def generate_summary(data: dict) -> str:
+    chat_completions = openai_client.chat.completions.create(
+        top_p=1,
+        temperature=0.5,
+        frequency_penalty=0.3,
+        presence_penalty=0.1,
+        model="gpt-4.1-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a world-class crypto expert specialising in writing report based on provided data by user. You should only write based on data provided by user, don't use any other data. Don't limit words, if you need to write a little more text, do it. Use simple language, the final user is a average human. If you are not confident, don't use information or claim.",
+            },
+            {"role": "user", "content": json.dumps(data)},
+        ],
+    )
+    summary = chat_completions.choices[0].message.content
+    return summary
+
+
 def main():
     market_data_adapter = CoinLoreMarketDataAdapter()
     general_asset_metadata = market_data_adapter.get_asset_metadata(Asset.SUI)
-    print("Asset name: ", general_asset_metadata.name)
-    print("Unit price category: ", unit_price_indicator(general_asset_metadata.price))
-    print(
-        "Market Cap Category: ", market_cap_indicator(general_asset_metadata.market_cap)
-    )
-    print(
-        "Dilution Risk: ",
-        dilution_risk_indicator(
-            total_supply=general_asset_metadata.total_supply,
-            circulating_supply=general_asset_metadata.circulating_supply,
+
+    data = {
+        "asset_name": general_asset_metadata.name,
+        "unit_price_category": unit_price_indicator(general_asset_metadata.price),
+        "market_cap_category": market_cap_indicator(general_asset_metadata.market_cap),
+        "dilution_risk": dilution_risk_indicator(
+            general_asset_metadata.total_supply,
+            general_asset_metadata.circulating_supply,
         ),
-    )
+    }
+    summary = generate_summary(data)
+    print(summary)
 
 
 if __name__ == "__main__":
